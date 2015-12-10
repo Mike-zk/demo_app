24a1b8ab232eac7d4d266f57347f9f3e20ee4c2b
saasbook <saasbook@saasbook.(none)>
2015-12-09 22:13:03 -0400

    Added name and email validation to the User model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
   attr_accessible :emails, :name
+  emails_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :name, :presence => true, :length => { :maximum => 50}
+  validates :emails, :presence =>true, 
+                     :format => { :with => emails_regex},
+                     :uniqueness => { :case_sensitive => false}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20151027061218) do
+ActiveRecord::Schema.define(:version => 20151210015926) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
@@ -19,5 +19,7 @@ ActiveRecord::Schema.define(:version => 20151027061218) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
+
+  add_index "users", ["emails"], :name => "index_users_on_emails", :unique => true
 
 end
